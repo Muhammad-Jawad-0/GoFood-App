366b09c7d21889722e6569e25640f55b10d39978
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-09-20 01:30:04 +0500

    live url added

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  // const url = "http://localhost:4000";
+  const url = "https://goofood-server-production.up.railway.app";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
