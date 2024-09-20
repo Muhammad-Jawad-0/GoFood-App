c5bc2c9103b9d74c98f5b09bfb051192648c58ce
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-09-21 00:55:09 +0500

    live url added in orderController file

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173" || "http://localhost:5174/";
+  const frontend_url = "http://localhost:5173" || "http://localhost:5174/ " || "https://goofood-server-production.up.railway.app";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
